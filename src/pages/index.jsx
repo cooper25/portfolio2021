aa8f60783e39f0486f390518fadf6b81487db880
cooper25 <cooper@cppr.dev>
2021-05-19 21:49:04 -0500

    🎉 New Portfolio

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,16 @@ import Helmet from "react-helmet"
 import "../components/main.css"
 import gallery from "../components/gallery.json"
 import cooper from "../images/cooper.png"
+//import icons (i know theres a better way but it works)
+import directory from "../images/icons/directory.png"
+import discord from "../images/icons/discord.png"
+import github from "../images/icons/github.png"
+import twitter from "../images/icons/twitter.png"
+import behance from "../images/icons/behance.png"
 
 // Choose Random Project
 var objects = gallery.length + 1
-var number = Math.floor(Math.random() * objects)
-var data = gallery[number]
+var data = gallery[Math.floor(Math.random() * objects)]
 
 const IndexPage = () => {
   return (
@@ -24,11 +29,15 @@ const IndexPage = () => {
           <p>Self-taught Graphic & Web Designer based out of the United States with over two years of experience.</p>
         </div>
         <div className="bottom-socials">
-          test
+          <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://directory.cppr.dev/"><img draggable="false" src={directory} alt="directory"></img></a>
+          <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://discord.com/users/307273393600724994/"><img draggable="false" src={discord} alt="discord"></img></a>
+          <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://github.com/cooper25"><img draggable="false" src={github} alt="github"></img></a>
+          <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://twitter.com/loertis_"><img draggable="false" src={twitter} alt="twitter"></img></a>
+          <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://behance.net/coopertsoris"><img draggable="false" src={behance} alt="behance"></img></a>
         </div>
       </div>
       <div className="right_image-container">
-        <div className="right_image" style={{backgroundImage: `url(${data.projectImage})`}}></div>
+        <div className="right_image" style={{backgroundImage: `url("${data.projectImage}")`}}></div>
         <footer>View <a target="_blank" rel="noopener noreferrer" href={data.projectLink}>{data.projectName}</a> on <b>Behance</b></footer>
       </div>
     </body>
