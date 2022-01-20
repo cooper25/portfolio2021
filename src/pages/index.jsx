804c935df0f15d1ed94ca2812bc83fee00839534
Cooper <cooper@cppr.dev>
2022-01-19 22:02:48 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,6 +37,7 @@ const IndexPage = () => {
         <link rel="shortcut icon" href={favicon}/>
         <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "1a7b87ab2d484241818047ef8bf2771d"}'></script>
         <script defer src='https://http://cooper25.github.io/hotjar.js'></script>
+        <script defer data-domain="cppr.dev" src="https://tsor.is/js/plausible.js"></script>
       </Helmet>
       <div className="screen-on-load">
         <img draggable="false" alt="cooper tsoris" src={cooper} height="35"/>
