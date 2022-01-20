050e8f58d2c50814a43c9bb8032f25dd2f309caa
Cooper Tsoris <sobczakcooper@gmail.com>
2022-01-19 23:51:55 -0600

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,12 @@ module.exports = {
   siteMetadata: {
     title: "portfolio2021",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-react-helmet"
+    {
+      resolve: `gatsby-plugin-plausible`,
+      options: {
+        domain: `tsor.is:8000`,
+      },
+    },
+  ],
 };
