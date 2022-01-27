934780b585265e0166101fc1487752bf6e8bd34b
Cooper Tsoris <sobczakcooper@gmail.com>
2022-01-26 20:55:40 -0600

    Plausible.io

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "portfolio2021",
   },
-  plugins: ["gatsby-plugin-react-helmet"
-    {
-      resolve: `gatsby-plugin-plausible`,
-      options: {
-        domain: `tsor.is:8000`,
-      },
-    },
-  ],
+  plugins: ["gatsby-plugin-react-helmet"]
 };
